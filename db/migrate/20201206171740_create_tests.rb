fef7d803a1ff89bcab535a7f221a562af0d85626
JuicyLucifer <56612012+JuicyLucfier@users.noreply.github.com>
2020-12-06 21:06:57 +0300

    Создал модели и миграции

---
+++ db/migrate/20201206171740_create_tests.rb
@@ -0,0 +1,10 @@
+class CreateTests < ActiveRecord::Migration[5.2]
+  def change
+    create_table :tests do |t|
+      t.string :title, null: false
+      t.integer :level, default: 1
+
+      t.timestamps
+    end
+  end
+end
