8b67bcee6ba0c38fb2a30562b215ce90c92bada7
JuicyLucifer <56612012+JuicyLucfier@users.noreply.github.com>
2020-12-07 20:00:42 +0300

    Обновил схему бд

--- db/schema.rb
+++ db/schema.rb
@@ -15,8 +15,10 @@ ActiveRecord::Schema.define(version: 2020_12_07_105128) do
   create_table "answers", force: :cascade do |t|
     t.string "title", null: false
     t.boolean "correct", default: false
+    t.integer "question_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
   end
 
   create_table "categories", force: :cascade do |t|
@@ -27,15 +29,21 @@ ActiveRecord::Schema.define(version: 2020_12_07_105128) do
 
   create_table "questions", force: :cascade do |t|
     t.string "title", null: false
+    t.integer "test_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["test_id"], name: "index_questions_on_test_id"
   end
 
   create_table "tests", force: :cascade do |t|
     t.string "title", null: false
-    t.integer "level", default: 1
+    t.integer "level", default: 1, null: false
+    t.integer "category_id", null: false
+    t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_tests_on_category_id"
+    t.index ["user_id"], name: "index_tests_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
