76bbabcf0ea43ee3eb292d7afe1578a1578cc7dc
JuicyLucifer <56612012+JuicyLucfier@users.noreply.github.com>
2020-12-10 18:41:11 +0300

    refactor

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,5 +1,8 @@
 class Test < ApplicationRecord
   def self.names_by_category(category_title)
-    order(title: :desc).joins('INNER JOIN categories ON categories.id = tests.category_id').where(categories: { title: category_title } ).pluck(:title)
+    joins('INNER JOIN categories ON categories.id = tests.category_id')
+      .where(categories: { title: category_title } )
+      .order(title: :desc)
+      .pluck(:title)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  def tests_by_level(test_level)
+  def tests_by_level_completed(test_level)
     Test.joins('INNER JOIN user_tests ON user_tests.test_id = tests.id').where(tests: { level: test_level }, user_tests: { user_id: id })
   end
 end
