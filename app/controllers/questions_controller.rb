bc8736cc8d71903ea136bc9939a1f52553d50d8c
JuicyLucifer <56612012+JuicyLucfier@users.noreply.github.com>
2020-12-25 02:16:53 +0300

    исправил проверку сохранения

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,7 @@
 class QuestionsController < ApplicationController
 
   before_action :find_question, only: %i[show destroy]
-  before_action :find_test, only: %i[index create]
+  before_action :find_test, only: %i[index new create]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
@@ -18,8 +18,13 @@ class QuestionsController < ApplicationController
   end
 
   def create
-    @question = @test.questions.create(question_params)
-    redirect_to action: :index if @question.errors.empty?
+    @question = @test.questions.new(question_params)
+
+    if @question.save
+      redirect_to :action => "show", :id => @question.id
+    else
+      render :new
+    end
   end
 
   def destroy
